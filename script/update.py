dc01e14826f385c4201ced1ad42bcc9d29cee5db
vagmr <77198027+vagmr@users.noreply.github.com>
2023-06-10 01:06:38 +0800

    Update update.py

--- script/update.py
+++ script/update.py
@@ -1,24 +1,32 @@
 from github import Github
 import requests
+import random
+import time
 
-# 创建GitHub对象
-g = Github("access_token")
+# 名人名言 API 接口
+api_url = "https://v1.hitokoto.cn/"
+params = {
+    "c": "i",
+    "encode": "text"
+}
+
+# 创建 GitHub 对象
+g = Github("your_access_token")
 
 # 获取仓库和文件信息
 repo = g.get_repo("your_username/your_repository")
-file_path = "Guestbook/script/your_file_name.py" # 实际的脚本路径
+file_path = "Guestbook/script/your_file_name.md" # 修改为实际的文件路径
 file = repo.get_contents(file_path)
 
-# 运行脚本
-exec(file.decoded_content.decode('utf-8'))
+# 随机获取一条名言
+response = requests.get(api_url, params=params)
+quote = response.text.strip()
 
 # 图片链接
 image_url = "https://source.unsplash.com/960x640/?perseverance&victory"
 
-# 下载图片并上传到GitHub
-response = requests.get(image_url)
-repo.create_file("dream.jpg", "commit message", response.content)
+# 创建 md 文件内容
+new_content = f"## {time.strftime('%B %dth | %m月%d号')}\n\n### {quote}\n\n`{quote}`\n\n---\n<div style=\"background-color:#90ccd9;border-radius:5px;padding:10px;color:black;\">\n无论遇到多大困难和挫折，都要坚持下去，因为成功的胜利就在不远处等待着你的到来。\n</div>\n\n---\n\n![dream image]({image_url})"
 
-# 更新md文件
-new_content = f"{quote}\n\n![perseverance and victory]({image_url})"
+# 更新 md 文件
 repo.update_file(file.path, "commit message", new_content, file.sha)
